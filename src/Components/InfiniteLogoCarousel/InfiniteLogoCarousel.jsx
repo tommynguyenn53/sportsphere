d820abedbc0fd93c47d8333de3105d4a978d4a32
Tommy Nguyen <tong9587@uni.sydney.edu.autong9587@uni.sydney.edu.autong9587@uni.sydney.edu.au>
2025-03-29 15:31:07 +1100

    Fix image paths and verify backend setup

--- src/Components/InfiniteLogoCarousel/InfiniteLogoCarousel.jsx
+++ src/Components/InfiniteLogoCarousel/InfiniteLogoCarousel.jsx
@@ -5,61 +5,48 @@ function InfiniteLogoCarousel(props) {
     return (
         <div className="logos">
             <div className="logos-slide">
-                <img src="../../../public/FIFA_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nba_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nfl-1.png" style={{height: "70px"}}/>
-                <img src="../../../public/pga-tour-6-logo-svg-vector.svg" style={{height: "70px"}}/>
-                <img src="../../../public/F1_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/wimbledon-1.png" style={{height: "60px"}}/>
-                <img src="../../../public/Mlb_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/NHL_Logo.svg" style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/FIFA_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nba_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nfl-1.png`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/pga-tour-6-logo-svg-vector.svg`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/F1_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/wimbledon-1.png`} style={{height: "60px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/Mlb_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/NHL_Logo.svg`} style={{height: "70px"}}/>
             </div>
 
             <div className="logos-slide">
-                <img src="../../../public/FIFA_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nba_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nfl-1.png" style={{height: "70px"}}/>
-                <img src="../../../public/pga-tour-6-logo-svg-vector.svg" style={{height: "70px"}}/>
-                <img src="../../../public/F1_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/wimbledon-1.png" style={{height: "60px"}}/>
-                <img src="../../../public/Mlb_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/NHL_Logo.svg" style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/FIFA_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nba_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nfl-1.png`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/pga-tour-6-logo-svg-vector.svg`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/F1_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/wimbledon-1.png`} style={{height: "60px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/Mlb_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/NHL_Logo.svg`} style={{height: "70px"}}/>
             </div>
 
             <div className="logos-slide">
-                <img src="../../../public/FIFA_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nba_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nfl-1.png" style={{height: "70px"}}/>
-                <img src="../../../public/pga-tour-6-logo-svg-vector.svg" style={{height: "70px"}}/>
-                <img src="../../../public/F1_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/wimbledon-1.png" style={{height: "60px"}}/>
-                <img src="../../../public/Mlb_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/NHL_Logo.svg" style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/FIFA_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nba_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nfl-1.png`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/pga-tour-6-logo-svg-vector.svg`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/F1_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/wimbledon-1.png`} style={{height: "60px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/Mlb_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/NHL_Logo.svg`} style={{height: "70px"}}/>
             </div>
 
             <div className="logos-slide">
-                <img src="../../../public/FIFA_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nba_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nfl-1.png" style={{height: "70px"}}/>
-                <img src="../../../public/pga-tour-6-logo-svg-vector.svg" style={{height: "70px"}}/>
-                <img src="../../../public/F1_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/wimbledon-1.png" style={{height: "60px"}}/>
-                <img src="../../../public/Mlb_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/NHL_Logo.svg" style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/FIFA_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nba_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/nfl-1.png`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/pga-tour-6-logo-svg-vector.svg`} style={{height: "70px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/F1_Logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/wimbledon-1.png`} style={{height: "60px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/Mlb_logo.svg`} style={{height: "50px"}}/>
+                <img src={`${process.env.PUBLIC_URL}/NHL_Logo.svg`} style={{height: "70px"}}/>
             </div>
-
-            <div className="logos-slide">
-                <img src="../../../public/FIFA_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nba_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/nfl-1.png" style={{height: "70px"}}/>
-                <img src="../../../public/pga-tour-6-logo-svg-vector.svg" style={{height: "70px"}}/>
-                <img src="../../../public/F1_Logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/wimbledon-1.png" style={{height: "60px"}}/>
-                <img src="../../../public/Mlb_logo.svg" style={{height: "50px"}}/>
-                <img src="../../../public/NHL_Logo.svg" style={{height: "70px"}}/>
-            </div>
-
-
         </div>
     );
 }
