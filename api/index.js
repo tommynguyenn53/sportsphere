6d43d786e3787b93f943f0594ca020312eec858e
Tommy Nguyen <tong9587@uni.sydney.edu.autong9587@uni.sydney.edu.autong9587@uni.sydney.edu.au>
2025-03-29 16:28:10 +1100

    Testing deploying backend

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getStats } from './sportspheremodel.js'; // Function to fetch results
+import { getStats } from '../sportspheremodel.js'; // Function to fetch results
 
 const app = express();
 const port = 3001;
